5ce1b1e5c830341e3747ba8648fbaf0b38d932ea
CheonGroo <09-25@naver.com>
2026-08-06 17:38:40 +0900

    0806 JS

--- js/0806연습.js
+++ js/0806연습.js
@@ -67,3 +67,21 @@ console.log(`${number} isn't ${typeof number} data type.`);
 
 //명시적 형변환
 //parseInt 정수만 출력 , parseFloat 실수만 출력, Boolean 참/거짓 출력
+
+let mathScore = "77";
+let engScore = "88";
+
+let avgScore = (Number(mathScore) + Number(engScore)) / 2;
+console.log(
+  `수학점수 : ${mathScore}, 영어점수 : ${engScore}, 평균점수 : ${avgScore}`,
+);
+
+//증가 감소 연산자
+let a1 = 10;
+let a2 = 10;
+
+let result1 = a1++;
+let result2 = ++a2;
+
+console.log(result1, a1);
+console.log(result2, a2);
